48161f86735049c52dc6bdc70854bffebddf61dc
Natsurii <anounymousandrei@gmail.com>
2024-03-13 15:23:06 +0800

    ✨ feature: add cerebro color schemes

---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+// colors.dart
+// define color constants fo the cerebro design theme
+// Author: Andrei Drice
+// March, 13, 2024
+import 'package:flutter/material.dart';
+
+const cerebroBlue100 = Color.fromRGBO(25, 25, 112, 1);
+const cerebroBlue200 = Color.fromRGBO(0, 84, 166, 1);
+const cerebroBlue300 = Color.fromRGBO(16, 71, 173, 1);
+const cerebroWhite = Color.fromRGBO(255, 255, 255, 1);
