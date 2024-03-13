e291d44358cb18d9393fe605733d0eb8dd2e48ca
Natsurii <anounymousandrei@gmail.com>
2024-03-13 15:22:36 +0800

    ✅ feature: add text themes

---
+++ lib/theme/texts.dart
@@ -0,0 +1,146 @@
+// texts.dart
+// define text constants fo the cerebro design theme
+// Author: Andrei Drice
+// March, 13, 2024
+
+import 'package:flutter/material.dart';
+
+// =================================
+//
+//           P O P P I N S
+//
+// ==================================
+
+
+const poppinsTitle =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w900,
+  fontSize: 48.0,
+);
+
+const poppinsSubitle =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w300,
+  fontSize: 24.0,
+);
+
+const poppinsH1 =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w700,
+  fontSize: 36.0,
+);
+
+const poppinsH2 =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w700,
+  fontSize: 32.0,
+);
+
+const poppinsH3 =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w700,
+  fontSize: 26.0,
+);
+
+const poppinsH4 =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w700,
+  fontSize: 24.0,
+);
+
+const poppinsH5 =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w700,
+  fontSize: 20.0,
+);
+
+const poppinsH6 =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w700,
+  fontSize: 16.0,
+);
+
+const poppinsParagraph =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w400,
+  fontSize: 16.0,
+);
+
+const poppinsQuote =  TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w400,
+  fontSize: 16.0,
+  fontStyle: FontStyle.italic,
+);
+
+
+// =================================
+//
+//        M O N T S E R R A T
+//
+// ==================================
+
+
+const montserratTitle =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w900,
+  fontSize: 48.0,
+);
+
+const montserratSubitle =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w300,
+  fontSize: 24.0,
+);
+
+const montserratH1 =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+  fontSize: 36.0,
+);
+
+const montserratH2 =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+  fontSize: 32.0,
+);
+
+const montserratH3 =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+  fontSize: 26.0,
+);
+
+const montserratH4 =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+  fontSize: 24.0,
+);
+
+const montserratH5 =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+  fontSize: 20.0,
+);
+
+const montserratH6 =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+  fontSize: 16.0,
+);
+
+const montserratParagraph =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w400,
+  fontSize: 16.0,
+);
+
+const montserratQuote =  TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w400,
+  fontSize: 16.0,
+  fontStyle: FontStyle.italic,
+);
+
+
+
