b0a642fe18eb3fb23006d9e6346ace903c19ac02
Natsurii <anounymousandrei@gmail.com>
2024-03-13 15:23:55 +0800

    ✨ feature: add cerebro theme

---
+++ lib/theme/cerebro_theme.dart
@@ -0,0 +1,10 @@
+// cerebro_theme.dart
+// Cerebro Design Theme imports
+// Author: Andrei Drice
+// March 13, 2024
+// ignore_for_file: unused_import
+import 'package:cerebro_mobile/theme/colors.dart';
+import 'package:cerebro_mobile/theme/dimensions.dart';
+import 'package:cerebro_mobile/theme/icons.dart';
+import 'package:cerebro_mobile/theme/texts.dart';
+
