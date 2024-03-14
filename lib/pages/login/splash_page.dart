beee64470d462d1bd5ff7717b6d74a8ede608d62
Natsurii <anounymousandrei@gmail.com>
2024-03-14 10:22:32 +0800

    💫 Add fade in animation in cerebro logo

--- lib/pages/login/splash_page.dart
+++ lib/pages/login/splash_page.dart
@@ -1,8 +1,6 @@
 import 'package:cerebro_mobile/theme/colors.dart';
 import 'package:flutter/material.dart';
 
-
-
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
@@ -22,13 +20,48 @@ class SplashPage extends StatelessWidget {
           )
         ),
 
-        child:Center(
-          child: Image(
-            image: AssetImage('assets/images/cerebro-splash.png'),
-          ),
-        ),
+        child: CerebroLogo(),
       ),
     );
   }
 }
 
+
+class CerebroLogo extends StatefulWidget {
+  const CerebroLogo({super.key});
+
+  @override
+  State<CerebroLogo> createState() => _CerebroLogoState();
+}
+
+class _CerebroLogoState extends State<CerebroLogo> {
+
+  double opacityLevel = 0.0;
+  
+  void _changeOpacity(){
+    setState(() =>  opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
+  }
+  
+   @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      setState(() {
+        _changeOpacity();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+          child: AnimatedOpacity(
+            opacity: opacityLevel,
+            duration: Duration(seconds: 3),
+            child: Image(
+              image: AssetImage('assets/images/cerebro-splash.png'),
+            ),
+          ),
+        );
+  }
+}
