fcbd60478d2533faba4443b8fa67c939f5e66b7c
Marc Ernest Mariano <78241292+marcernest21@users.noreply.github.com>
2024-03-25 12:39:56 +0800

    Feature Implement: Navigation Drawer

--- lib/organisms/cerebro_appbar.dart
+++ lib/organisms/cerebro_appbar.dart
@@ -1,52 +1,49 @@
-import 'package:cerebro_mobile/theme/colors.dart';
-import 'package:cerebro_mobile/theme/texts.dart';
 import 'package:flutter/material.dart';
 
 class CerebroAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   const CerebroAppBar({
-    super.key,
+    Key? key,
     required this.title,
-  });
+  }) : super(key: key);
 
   @override
-  Size get preferredSize => Size.fromHeight(92); // Height set to 92
+  Size get preferredSize => Size.fromHeight(92);
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
     return AppBar(
       leading: IconButton(
-        icon: Icon(Icons.menu, color: cerebroWhite, size: 40), // Hamburger menu icon
-        onPressed: () {}, // Handle menu opening logic here
+        icon: Icon(Icons.menu, color: Colors.white, size: 40),
+               onPressed: () {
+          // Open the navigation drawer using Scaffold.of(context).openDrawer()
+          Scaffold.of(context).openDrawer();
+        },
       ),
-      title: Row( // Use Row to position title and leading together
-        children: [
-          Text(
-            title,
-            style: poppinsH5.copyWith(
-              color: cerebroWhite,
-            ),
-          ),
-        ],
-      ),
+      title: Text(title, style: TextStyle(color: Colors.white)), // White title text
       flexibleSpace: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color.fromRGBO(0, 84, 166, 1), Color.fromRGBO(54, 116, 175, 1)],
+            colors: [
+              Color.fromRGBO(0, 84, 166, 1),
+              Color.fromRGBO(54, 116, 175, 1),
+            ],
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
           ),
         ),
       ),
       actions: [
-        TextButton( // Use TextButton instead of IconButton
+        TextButton(
           onPressed: () {}, // Handle profile actions here
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              width: 40, // Adjust width as needed
-              height: 40, // Adjust height as needed
-              clipBehavior: Clip.antiAlias, // Optional: improve image rendering
+              width: 40,
+              height: 40,
+              clipBehavior: Clip.antiAlias, // Improve image rendering
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 image: DecorationImage(
@@ -57,7 +54,6 @@ class CerebroAppBar extends StatelessWidget implements PreferredSizeWidget {
             ),
           ),
         ),
-        // Include any additional actions
       ],
       toolbarHeight: 92,
     );
