74ae7071685bb953ea57314637b5a5177433d55a
Marc Ernest Mariano <78241292+marcernest21@users.noreply.github.com>
2024-04-15 03:50:36 +0800

    Improvement Student Request Form Page
    Added new field for description when the option "Others" is selected.

--- lib/pages/request_page/request_form.dart
+++ lib/pages/request_page/request_form.dart
@@ -8,47 +8,74 @@ class RequestForm extends StatefulWidget {
 
 class RequestFormState extends State<RequestForm> {
   String? selectedRequest; // Change the type to nullable String
+  String? requestDescription;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 16.0),
-            Text('Choose only one (1) type of request per transaction'),
-            SizedBox(height: 16.0),
-            Container(
-              width: double.infinity, // Take full width of screen
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8), // Add padding
-              decoration: BoxDecoration(
-                border: Border.all(), // Add border for dropdown field look
-                borderRadius: BorderRadius.circular(8.0), // Rounded corners
-              ),
-              child: DropdownButtonHideUnderline(
-                // Hide the default underline
-                child: DropdownButton<String>(
-                  isExpanded: true, // Expand dropdown button to full width
-                  value: selectedRequest, // Set to null initially
-                  hint: Text('Select'), // Set hint text
-                  onChanged: (value) => setState(() => selectedRequest = value),
-                  items: ['Transcript of Records', 'Report Card', 'Other']
-                      .map((request) => DropdownMenuItem<String>(
-                          value: request, child: Text(request)))
-                      .toList(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 16.0),
+              Text('Choose only one (1) type of request per transaction'),
+              SizedBox(height: 16.0),
+              Container(
+                width: double.infinity, // Take full width of screen
+                padding: EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 8), // Add padding
+                decoration: BoxDecoration(
+                  border: Border.all(), // Add border for dropdown field look
+                  borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                ),
+                child: DropdownButtonHideUnderline(
+                  // Hide the default underline
+                  child: DropdownButton<String>(
+                    isExpanded: true, // Expand dropdown button to full width
+                    value: selectedRequest, // Set to null initially
+                    hint: Text('Select'), // Set hint text
+                    onChanged: (value) {
+                      setState(() {
+                        selectedRequest = value;
+                        // Clear request description when a different option is selected
+                        requestDescription = null;
+                      });
+                    },
+                    items: ['Transcript of Records', 'Report Card', 'Other']
+                        .map((request) => DropdownMenuItem<String>(
+                            value: request, child: Text(request)))
+                        .toList(),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 24.0),
-            Center(
-              child: CerebroElevatedBtn(
-                onPressed: () => print('Request submitted: $selectedRequest'),
-                text: ('Submit Request'),
+              // Show request description field only if 'Other' is selected
+              if (selectedRequest == 'Other') ...[
+                SizedBox(height: 16.0),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Request Description/Remarks',
+                    hintText: 'Example: Request for a copy of my Diploma',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      requestDescription = value;
+                    });
+                  },
+                ),
+              ],
+              SizedBox(height: 24.0),
+              Center(
+                child: CerebroElevatedBtn(
+                  onPressed: () => print(
+                      'Request submitted: $selectedRequest\nDescription: $requestDescription'),
+                  text: 'Submit Request',
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
