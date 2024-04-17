de4b568a8e4f0ec9f2daf7e3f58fe0e268b8ab64
Marc Ernest Mariano <78241292+marcernest21@users.noreply.github.com>
2024-04-17 15:09:55 +0800

    Teacher Navigation Drawer

---
+++ lib/atoms/teacher_navdrawer.dart
@@ -0,0 +1,91 @@
+import 'package:cerebro_mobile/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class TeacherNavigationDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.65, // Adjust the width as needed
+      child: Drawer(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                cerebroBlue200, Color.fromRGBO(102, 143, 183, 1)
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+          child: ListView(
+            padding: EdgeInsets.only(left:20),
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      cerebroBlue200, Color.fromRGBO(102, 143, 183, 1)
+                    ],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/SchoolLogo.png'),
+                      radius: 30,
+                    ),
+                    SizedBox(width: 20),
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'ABC School',
+                            style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'of Cavite',
+                            style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.dashboard, color: Colors.white),
+                title: Text('Dashboard', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  //Navigator.push(context,
+                      //MaterialPageRoute(builder: (context) => DashboardPage())); // Navigate to Teachers Dashboard
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.person, color: Colors.white),
+                title: Text('Profile', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  //Navigator.push(context,
+                      //MaterialPageRoute(builder: (context) => EditProfile())); // Navigate to Teacher Profile
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.computer_rounded, color: Colors.white),
+                title: Text('My Classes', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  //Navigator.push(context,
+                      //MaterialPageRoute(builder: (context) => MyClassesPage())); // Navigate to My Classes Page
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
