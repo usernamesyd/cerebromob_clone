7582af703f4ad1109bc319056747a6f328561e5f
maristelaa <angiemaristela71@gmail.com>
2024-05-09 09:18:44 +0800

    Enhancement on backend of change profile and profile screen
    Commented out the backend code, the only thing that work here is the browsing function of image on users gallery and displaying it on the avatar.

--- lib/pages/student/profile_settings/change_profile.dart
+++ lib/pages/student/profile_settings/change_profile.dart
@@ -1,11 +1,14 @@
 import 'package:cerebro_mobile/atoms/cerebro_elevated_btn.dart';
 import 'package:cerebro_mobile/atoms/navigation_drawer.dart';
 import 'package:cerebro_mobile/organisms/cerebro_appbar.dart';
-//import 'package:cerebro_mobile/pages/login/login_page.dart';
+import 'package:cerebro_mobile/pages/student/profile_settings/profile_screen.dart';
 import 'package:cerebro_mobile/theme/colors.dart';
 import 'package:cerebro_mobile/theme/texts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:firebase_database/firebase_database.dart';
 import 'dart:io';
 
 class ChangeProfile extends StatefulWidget {
@@ -16,10 +19,34 @@ class ChangeProfile extends StatefulWidget {
 }
 
 class _ChangeProfileState extends State<ChangeProfile> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  //final databaseReference = FirebaseDatabase.instance.reference().child('users');
   File? _image;
+  String? _imageUrl;
   final picker = ImagePicker();
 
-  Future pickImage() async {
+  /*@override
+  void initState() {
+    super.initState();
+    fetchProfilePictureUrl();
+  }*/
+
+
+   /*Future<String> uploadImage(File imageFile) async {
+    try {
+      final filename = '${DateTime.now().millisecondsSinceEpoch}.${imageFile.path.split('.').last}';
+      final storageRef = FirebaseStorage.instance.ref().child('profile_images/$filename');
+      final uploadTask = storageRef.putFile(imageFile);
+      await uploadTask;
+      final imageUrl = await storageRef.getDownloadURL();
+      return imageUrl;
+    } catch (error) {
+      print('Error uploading image: $error');
+      rethrow;
+    }
+  }*/
+  //function saving selected image
+  Future<void> pickImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
@@ -29,6 +56,78 @@ class _ChangeProfileState extends State<ChangeProfile> {
     }
   }
 
+  String? get currentUserId => auth.currentUser?.uid;
+
+  /*Future<void> saveProfile() async {
+    if (_image == null) return;
+
+    try {
+      final imageUrl = await uploadImage(_image!);
+      final userUpdates = <String, String>{
+        'profile_picture': imageUrl,
+      };
+
+      if (auth.currentUser?.uid != null) {
+        await databaseReference.child(auth.currentUser!.uid).update(userUpdates);
+
+        // Set the newly uploaded image URL as the profile picture URL
+        setState(() {
+          _imageUrl = imageUrl;
+        });
+
+        // Show success dialog
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Success!'),
+            content: Text('Your profile picture has been uploaded successfully.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+      } else {
+        print('User not logged in. Cannot update profile picture.');
+      }
+    } catch (error) {
+      print('Error saving image URL to database: $error');
+      // Show error dialog
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('An error occurred while saving profile picture details. Please try again.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }*/
+
+  /*Future<void> fetchProfilePictureUrl() async {
+  try {
+    // Using `onValue` instead of `once` to listen for changes continuously
+    databaseReference.child(currentUserId!).onValue.listen((event) {
+      Map<dynamic, dynamic>? values = (event.snapshot.value as Map<dynamic, dynamic>?);
+      if (values != null) {
+        setState(() {
+          _imageUrl = values['profile_picture'];
+        });
+      }
+    });
+  } catch (error) {
+    print('Error fetching profile picture URL: $error');
+  }
+}*/
+
+  //start of structure styling
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,23 +154,51 @@ class _ChangeProfileState extends State<ChangeProfile> {
                 padding: const EdgeInsets.only(bottom: 16.0),
                 child: Column(
                   children: [
-                    CircleAvatar(
+                     CircleAvatar(
                       radius: 100,
                       backgroundImage: _image != null
                           ? FileImage(_image!)
-                          : AssetImage('assets/images/profile.png') as ImageProvider,
+                          : (_imageUrl != null
+                              ? NetworkImage(_imageUrl!) 
+                              : AssetImage('assets/images/profile.png') as ImageProvider),
                     ),
                     SizedBox(height: 20),
-                    CerebroOutlinedBtn(
-                      onPressed: pickImage,
-                      text: 'Upload Image',
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      _image != null ? 'Image selected' : 'No image selected',
-                      style: TextStyle(fontSize: 14),
-                    ),
                   ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: SizedBox(
+                  width: 320,
+                  child: Container(
+                    padding: const EdgeInsets.all(10), // Adjust padding as needed
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: cerebroBlue300, // Outline border color
+                        width: 2, // Border width
+                      ),
+                      borderRadius: BorderRadius.circular(10), // Border radius
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CerebroOutlinedBtn(
+                              onPressed: pickImage,
+                              text: 'Upload Image',
+                            ),
+                            SizedBox(width: 14),
+                            Text(
+                              _image != null ? 'Image selected' : 'No image selected',
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
               Padding(
@@ -79,7 +206,7 @@ class _ChangeProfileState extends State<ChangeProfile> {
                 child: Column(
                   children: [
                     CerebroElevatedBtn(
-                      onPressed: () => {},
+                      onPressed: () => {},//saveProfile, //btn to save the changed profile
                       text: 'Save Changes',
                     ),
                   ],
@@ -88,9 +215,12 @@ class _ChangeProfileState extends State<ChangeProfile> {
               Padding(
                 padding: const EdgeInsets.only(bottom: 16.0),
                 child: Column(
-                  children: [
+                  children: [ 
                     CerebroOutlinedBtn(
-                      onPressed: () => {Navigator.pop(context)},
+                      onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => ProfileScreen()));
+                        },
                       text: 'Close',
                     ),
                   ],
